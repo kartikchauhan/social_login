8d28fcf8fe4d510d347647634370ecab2c84ca4a
kartik chauhan <chauhan.kartik25@gmail.com>
2017-02-04 02:23:06 +0530

    added google authentication - refer tutorials from codexworld.com and it's video on youtube for more information

---
+++ login_with_google_using_php/gpConfig.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+//Include Google client library 
+include_once 'src/Google_Client.php';
+include_once 'src/contrib/Google_Oauth2Service.php';
+
+/*
+ * Configuration and setup Google API
+ */
+$clientId = '1084607052036-i2q1bbu1qnheeftrvpla974le9k5lmrf.apps.googleusercontent.com'; //Google client ID
+$clientSecret = 'ImGI6dV5I4lH440IDkkDjVwK'; //Google client secret
+$redirectURL = 'http://localhost/social_login_example/login_with_google_using_php/index.php'; //Callback URL
+
+//Call Google API
+$gClient = new Google_Client();
+$gClient->setApplicationName('blogSparta');
+$gClient->setClientId($clientId);
+$gClient->setClientSecret($clientSecret);
+$gClient->setRedirectUri($redirectURL);
+
+$google_oauthV2 = new Google_Oauth2Service($gClient);
+?>
